333aec62e83b9e7eb4ad3967c8d13f5d186ec818
Devansh <devanshsoni79@gmail.com>
2023-06-23 16:16:12 +0530

    added a search icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { getData } from "./api/api";
 import { EmployeeData } from "./types";
 import ListItem from "./components/ListItem";
+import searchIcon from "./assets/search-icon.png"
 
 function getFilteredItems(query: string, items: EmployeeData[]) {
   if (!query) {
@@ -37,12 +38,16 @@ function App() {
     <>
       <h1 className="heading">Employee List</h1>
       <section className="container">
-        <input
-          type="text"
-          className="search_bar"
-          placeholder="Search..."
-          onChange={(e) => setQuery(e.target.value)}
-        />
+        <div className="search_container">
+          <input
+            type="text"
+            className="search_bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <img width="25" height="25" className="search_icon"  src={searchIcon} alt="search--v1"/>
+        </div>
+
         <section className="employee_list_container">
           {filteredEmployeeList?.map(
             ({ id, avatar, email, first_name, last_name }) => (
